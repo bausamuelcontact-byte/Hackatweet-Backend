03491a77a1bdb349c3f3a6ede854a68de71b6ffe
Houyam <houyamsed@gmail.com>
2025-11-20 11:00:23 +0100

    backend1

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+require('../models/connection');
+const User = require('../models/users');
+const { checkBody } = require('./modules/checkBody');
+const uid2 = require('uid2');
+const bcrypt = require('bcrypt');
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:token', function(req, res, next) {
+  User.findOne({ token: req.params.token }).then(data => {
+    if (data) {
+      res.json({ result: true });
+    } else {
+      res.json({ result: false, error: 'User not found' });
+    }
+  });
 });
 
 module.exports = router;
